5646b3b251e0f7ef6a42f39d9f6c1e1c5cbef89d
Marlin- <marlin-@gmx.cn>
2018-01-31 06:29:11 +0800

    Add helper functions

--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,24 @@
 
+#' @export
+PkgPath <- function () {
+    system.file(package = "Rlibsymengine", mustWork = TRUE)
+}
 
-NULL
+#' @export
+IPath <- function () {
+    system.file("include", package = "Rlibsymengine", mustWork = TRUE)
+}
+
+#' @export
+LPath <- function () {
+    pkg <- PkgPath()
+    # It may located at `lib` or `lib64`?
+    # TODO: test on MacOS and Windows?
+    if (length(list.files(file.path(pkg, "lib"), pattern = "^libsymengine\\.(a|lib)$")))
+        return(file.path(pkg, "lib"))
+    if (length(list.files(file.path(pkg, "lib64"), pattern = "^libsymengine\\.(a|lib)$")))
+        return(file.path(pkg, "lib64"))
+    stop("The static library is (may not be) not found in ",
+         dQuote(file.path(pkg, "lib")), " and ", dQuote(file.path(pkg, "lib64")))
+}
+
