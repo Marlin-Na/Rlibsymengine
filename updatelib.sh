3cc7b4265e35018765ebb68f4f41ac206e6abf1e
Marlin- <marlin-@gmx.cn>
2018-02-01 07:05:07 +0800

    Fix the regular expression...

--- updatelib.sh
+++ updatelib.sh
@@ -13,7 +13,7 @@ echo to:
 git subtree pull --prefix inst/symengine/ \
     git@github.com:symengine/symengine.git master --squash \
     2>&1 >/dev/null \
-    | grep "^Subtree is already at commit 58286ba6b8cb1d49934e2f3ac9f82cf6136a3e0a\\.$" \
+    | grep "^Subtree is already at commit ........................................\\.$" \
     | sed 's/^Subtree is already at commit //' \
     | sed 's/\.$//'
 
