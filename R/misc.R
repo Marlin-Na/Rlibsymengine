644e11742ec6ceef7c3b744d797a6da5b65f0dd0
Marlin- <marlin-@gmx.cn>
2018-01-31 18:29:20 +0800

    Helper function to provide all flags

--- R/misc.R
+++ R/misc.R
@@ -4,21 +4,43 @@ PkgPath <- function () {
     system.file(package = "Rlibsymengine", mustWork = TRUE)
 }
 
-#' @export
-IPath <- function () {
-    system.file("include", package = "Rlibsymengine", mustWork = TRUE)
-}
+# #' @export
+# IPath <- function () {
+#     system.file("include", package = "Rlibsymengine", mustWork = TRUE)
+# }
 
 #' @export
-LPath <- function () {
-    pkg <- PkgPath()
+PkgLibsFlags <- function () {
+    libs <- c("-lgmp", "-lmpfr", "-lmpc") # TODO: bfd, etc.
+
+    # Try to search for the static libraries, the location may be platform-dependent
+
+    pkg.path <- PkgPath()
+
+    # libsymengine.a, it locates at "lib64/libsymengine.a" on my computer
     # It may located at `lib` or `lib64`?
     # TODO: test on MacOS and Windows?
-    if (length(list.files(file.path(pkg, "lib"), pattern = "^libsymengine\\.(a|lib)$")))
-        return(file.path(pkg, "lib"))
-    if (length(list.files(file.path(pkg, "lib64"), pattern = "^libsymengine\\.(a|lib)$")))
-        return(file.path(pkg, "lib64"))
-    stop("The static library is (may not be) not found in ",
-         dQuote(file.path(pkg, "lib")), " and ", dQuote(file.path(pkg, "lib64")))
+    lsymengine <-
+        c(list.files(file.path(pkg.path, "lib"),
+                     pattern = "^libsymengine\\.(a|lib)$", full.names = TRUE),
+          list.files(file.path(pkg.path, "lib64"),
+                     pattern = "^libsymengine\\.(a|lib)$", full.names = TRUE))
+    if (length(lsymengine) != 1L)
+        stop(sprintf("Error finding the static library of symengine in %s", pkg.path))
+
+    # Try to find liblteuchos.a
+    lteuchos <-
+        c(list.files(file.path(pkg.path, "lib"),
+                     pattern = "^libteuchos\\.(a|lib)$", full.names = TRUE),
+          list.files(file.path(pkg.path, "lib64"),
+                     pattern = "^libteuchos\\.(a|lib)$", full.names = TRUE))
+    if (length(lteuchos) != 1L)
+        stop(sprintf("Error finding the static library of teuchos in %s", pkg.path))
+
+    # Expect to be the same?
+    stopifnot(dirname(lsymengine) == dirname(lteuchos))
+
+    ans <- c(libs, lsymengine, lteuchos)
+    ans
 }
 
